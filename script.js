80591ec406ef8a624106043013dcb45422dfa66f
Syed Masruf Ahmad <135939210+SyedMarufAhmad@users.noreply.github.com>
2023-09-26 18:33:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-// complete the given function
 
 function palindrome(str){
- let s = "";
-	str = str.toLowerCase();
- for (let i = str.length-1; i >=0; i--) {
- 	s+=str[i];
- }
-	if (s===str) {
-		return true;
+    let ch="" ;
+	str=str.toLowerCase();
+	for (let i = (str.length-1); i >=0 ; i--) {
+		ch+=str[i];
+	
 	}
-	else{
+	if(ch==str){
+		return true;
+	}else{
 		return false;
 	}
 }
